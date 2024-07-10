21f7765eabb0abd1a6eb6a84471f42005a2f89fd
asiphile <asiphilemthethethwa@gmail.com>
2024-07-10 15:51:22 +0200

    07/10/2024 update

--- src/components/app.js
+++ src/components/app.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import Card from './card';
+import Card from './Card';
+import './Card.css'
 import '../App.css'
 import chairImage from '../assets/chairImage-removebg-preview.png';
 import lightingImage from '../assets/lightBig-removebg-preview.png';
 import couchImage from '../assets/Sofa-Image-removebg-preview.png';
 
+// for import '../App.css' always remember to add the extention and two dots
+
 const App = () => {
   return (
     <div className="app">
-      <Card className='card1'
+      <Card
         image={chairImage}
         title="Stylish Chair"
         description="Chair with minimal style and modern. Wooden and plastic material."
         bgColor="#f3e4d3"
+        cardClass="card1"
       />
-      <Card className='card2'
+      <Card
         image={lightingImage}
         title="Lightings"
         description="Roof light and lamps we have wide range. In all colors and lamps also available."
         bgColor="#e0ebf7"
+        cardClass="card2"
       />
-      <Card className='card3'
+      <Card
         image={couchImage}
         title="Round shape couch"
         description="Stylish round shape couch is comfortable. Also available in multiple colors."
         bgColor="#f7f1c7"
+        cardClass="card3"
       />
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ image, title, description, bgColor }) => {
+const Card = ({ image, title, description, bgColor, cardClass }) => {
   return (
-    <div className="card" style={{ backgroundColor: bgColor }}>
+    <div className={`card ${cardClass}`} style={{ backgroundColor: bgColor }}>
       <img src={image} alt={title} className="card-image" />
       <h2 className="card-title">{title}</h2>
       <p className="card-description">{description}</p>
